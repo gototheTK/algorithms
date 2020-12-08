dca34ad0612a3bfa7b20ffcedd881fc8b9f4c673
gototheTK <48229162+gototheTK@users.noreply.github.com>
2020-12-08 18:09:24 +0900

    Create 3base_flip.java

---
+++ 3base_flip.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int n) {
+        String answer = "";
+        
+        while(n!=0){
+            
+            if(n%3==0){
+                answer+= 0;
+                n = n/3;
+            }else{
+                answer += n%3;
+                n -= n%3;
+                n = n/3;
+            }
+            
+        }
+        
+        int sum=0;
+        int pow = answer.length()-1;
+        for(String str : answer.split("")){
+            
+            int temp = Integer.parseInt(str);
+
+            
+            sum += Math.pow(3, pow)*temp;
+            
+            pow--;
+            
+        }
+        
+
+        return sum;
+    }
+}
