832721cfb105d34ff562329d1f2c355938622ccd
gototheTK <48229162+gototheTK@users.noreply.github.com>
2020-12-09 17:35:49 +0900

    123나라의 숫자

---
+++ 124numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        while(n!=0){
+            
+            
+            
+            if(n%3==0){
+                
+                answer = 4+answer;
+                n = n/3 - 1;
+            }else{
+                
+                if(n%3==2){
+                    answer = 2+answer;
+                }else if(n%3==1){
+                    answer = 1+answer;
+                }
+                n = n/3;
+            }
+            
+            
+            
+        }
+        
+        
+        return answer;
+    }
+}
