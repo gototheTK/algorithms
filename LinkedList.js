ad0bc2b72ba18911a345d63606986c18bf4e899b
gototheTK <gototkno1@gmail.com>
2022-06-14 03:50:14 +0900

    LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -32,12 +32,14 @@ function LinkedList() {
   };
 
   // Only change code below this line
-
+  // 주어진 인덱스를 찾아 새로운 값의 노드를 넣는다.
   this.addAt = function (index, element) {
+    // 이전 노드와 현재 노드를 가르키기 위한 변수 선언
     let current = head;
     let previous;
     let newNode = new Node(element);
 
+    // 값을 넣고자하는 노드의 인덱스가 0일 경우
     if (index === 0) {
       newNode.index = 0;
       newNode.next = head;
@@ -46,8 +48,8 @@ function LinkedList() {
       return;
     }
 
+    // 값을 넣고자하는 인덱스가 1일 경우
     while (index >= length && index > -1 && current.next) {
-      // 변수선언
       previous = current;
       current = current.next;
 
